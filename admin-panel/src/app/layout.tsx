6f53530395e9a2c526c7e270596cd6c1d67c7fb4
codexdhruv <dhruvsen24@gmail.com>
2026-09-11 17:51:39 +0530

    Refactor global CSS variables and base layout structure

--- admin-panel/src/app/layout.tsx
+++ admin-panel/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link 
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:wght@300;400;500;600;700&family=Manrope:wght@300;400;500;600;700;800&display=swap" 
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" 
           rel="stylesheet" 
         />
       </head>
